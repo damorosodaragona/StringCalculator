47bd8c8cd0a1a81412e81664c1c0ec55606b4232
Dario <Dario@Dario.lan>
2016-11-29 15:51:58 +0100

    Implementata la seconda regola e fatto refactoring

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,18 +1,27 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		return sum(numbersStr);
 	}
 
-	private int sum(String numberString) {
+	private int sum(String numberString) throws StringCalculatorException {
 
 		int value = 0;
 
+		if (numberString.contains(",\n") || numberString.contains("\n,")) {
+
+			throw new StringCalculatorException();
+
+		}
+
 		numberString = numberString.replace(",", "");
+		numberString = numberString.replace("\n", "");
 
 		for (int i = 0; i < numberString.length(); i++) {
+
 			value += Integer.parseInt(numberString.substring(i, i + 1));
+
 		}
 		return value;
 	}
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -5,49 +5,68 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
 
 public class StringCalculatorTest {
 
 	@Test
-	public void CalculatorForEmptyString() {
+	public void CalculatorForEmptyString() throws StringCalculatorException {
 		StringCalculator str = new StringCalculator();
 		assertEquals(0, str.add(""));
 	}
 	
 	@Test
-	public void CalculatorForStringWithNumber1() {
+	public void CalculatorForStringWithNumber1() throws StringCalculatorException {
 		StringCalculator str = new StringCalculator();
 		assertEquals(1, str.add("1"));
 	}
 	
 	@Test
-	public void CalculatorForStringWithNumber1and2() {
+	public void CalculatorForStringWithNumber1and2() throws StringCalculatorException {
 		StringCalculator str = new StringCalculator();
 		assertEquals(3, str.add("1,2"));
 	}
 	
 	@Test
-	public void CalculatorForStringWithNumber3and4() {
+	public void CalculatorForStringWithNumber3and4() throws StringCalculatorException {
 		StringCalculator str = new StringCalculator();
 		assertEquals(7, str.add("3,4"));
 	}
 	
 	@Test
-	public void CalculatorForStringWith3Numbers() {
+	public void CalculatorForStringWith3Numbers() throws StringCalculatorException {
 		StringCalculator str = new StringCalculator();
 		assertEquals(12, str.add("3,4,5"));
 	}
 	
 	@Test
-	public void CalculatorForStringWith4Numbers() {
+	public void CalculatorForStringWith4Numbers() throws StringCalculatorException {
 		StringCalculator str = new StringCalculator();
 		assertEquals(18, str.add("3,4,5,6"));
 	}
 	
 	@Test
-	public void CalculatorForStringWith10Numbers() {
+	public void CalculatorForStringWith10Numbers() throws StringCalculatorException {
 		StringCalculator str = new StringCalculator();
 		assertEquals(43, str.add("3,4,5,6,7,8,9,1,0"));
+	}
+	
+	@Test
+	public void CalculatorForStringWithSeparators() throws StringCalculatorException {
+		StringCalculator str = new StringCalculator();
+		assertEquals(7, str.add("3\n4"));
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void CalculatorForStringWithIllegalSeparators() throws StringCalculatorException {
+		StringCalculator str = new StringCalculator();
+		assertEquals(7, str.add("3,\n4"));
+	}
+	
+	@Test
+	public void CalculatorForStringWithSeparatorsAndMoreNumbers() throws StringCalculatorException {
+		StringCalculator str = new StringCalculator();
+		assertEquals(22, str.add("3\n4,5\n9,1"));
 	}
 
 }
